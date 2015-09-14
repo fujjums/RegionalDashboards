d0b47a079f72e1963485eb0604eccb4637effa94
fujjums <sho.fujiwara@gmail.com>
2015-09-14 16:02:37 -0400

    First commit

---
+++ server.R
@@ -0,0 +1,241 @@
+
+library(shiny)  
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(zoo)
+library(googleVis)
+library(zipcode)
+library(leaflet)
+setwd("./data/")
+Orders <- as.data.frame(read.csv("toMergeOrders.csv", stringsAsFactors=FALSE, na.strings = "NA"))
+#change dates from strings-> dates
+Orders$First.order <- as.Date(Orders$First.order, format = "%Y-%m-%d")
+Orders$Last.order <- as.Date(Orders$Last.order, format = "%Y-%m-%d")
+Orders$Pick.up.Date <- as.Date(Orders$Pick.up.Date, format = "%Y-%m-%d")
+Orders$order_week <- as.Date(Orders$order_week, format = "%Y-%m-%d")
+class(Orders$order_week)
+Orders$order_month <- as.Date(as.yearmon(Orders$order_week))
+Orders$order_year <- format(Orders$order_week, format="%Y")
+
+data(zipcode)
+Orders[c("Coupon", "Comp", "Ref.Receipt", "Ref.Sender")][is.na(Orders[c("Coupon", "Comp", "Ref.Receipt", "Ref.Sender")])] <- 0
+
+Orders$Zip.Code <- clean.zipcodes(Orders$Zip.Code)
+Orders <- inner_join(Orders, zipcode[c(1,4:5)], by = c("Zip.Code" = "zip"))
+
+Members <- as.data.frame(read.csv("toMergeMembers.csv", stringsAsFactors=FALSE, na.strings = "NA"))
+	
+Members$mem_first_order_week <- as.Date(Members$mem_first_order_week, format = "%Y-%m-%d")
+Members$mem_last_order_week <- as.Date(Members$mem_last_order_week, format = "%Y-%m-%d")
+
+Members$mem_first_order_month <- as.Date(as.yearmon(Members$mem_first_order_week))
+Members$mem_first_order_year <- format(Members$mem_first_order_week, format="%Y")
+
+CommunityWeeks <- as.data.frame(read.csv("toMergeCommunityWeeks.csv", stringsAsFactors=FALSE, na.strings = "NA"))
+CommunityWeeks$order_week <- as.Date(CommunityWeeks$order_week, format = "%Y-%m-%d")
+CommunityWeeks$order_month <- as.Date(as.yearmon(CommunityWeeks$order_week))
+CommunityWeeks$order_year <- format(CommunityWeeks$order_week, format="%Y")
+
+
+
+
+shinyServer(
+  function(input, output) {
+#
+#
+#ORDER TAB#    
+#
+#
+    
+#-------------------------------------------------
+#Reactive Functions (used by the render functions)
+#-------------------------------------------------
+    
+      orders_reactive_region <- reactive({
+          switch(input$orders_input_region,
+               "NY" = c("NY"),
+               "SF Bay" = c("SF Bay"),
+               "Both Regions" = c("NY","SF Bay"))  
+      })
+
+      orders_reactive_type <- reactive({
+          switch(input$orders_input_type,
+                "School" = c("SCHOOL"),
+                "Home/Work" = c("HOME", "WORKPLACE"),
+                "All" = c("SCHOOL","HOME", "WORKPLACE"))
+      })
+      
+      orders_reactive_coupon <- reactive({
+        if(input$orders_input_couponBool == "TRUE"){0}
+        else{-1}
+      })
+      
+      orders_reactive_comp <- reactive({
+        if(input$orders_input_couponBool == "TRUE"){0}
+        else{-1}
+      })
+      
+      orders_reactive_data <- reactive({
+        Orders %>%
+          filter(Region %in% orders_reactive_region(), Community.Type %in% orders_reactive_type(), 
+                 (Member.Order.Number >= input$orders_input_memberOrderNumber[1] &
+                    Member.Order.Number <= input$orders_input_memberOrderNumber[2] ),
+                 (Pick.up.Date >= input$orders_input_orderDate[1] &
+                    Pick.up.Date <= input$orders_input_orderDate[2] ),
+                 (Value >= input$orders_input_value[1] &
+                    Value <= input$orders_input_value[2]),
+                 Coupon > orders_reactive_coupon(),
+                 Comp > orders_reactive_comp()
+          )
+      })
+      
+      
+#-------------------------------------------------
+#Render Functions
+#-------------------------------------------------
+    #Data for Download Button
+      output$download_data <- downloadHandler(
+        filename = 'download.csv',
+        content = function(file) {
+          write.csv(orders_reactive_data(), file)
+        }
+      )
+      
+    #Summary Tab Output
+      output$orders_render_table_summary <- renderTable({
+          orders_reactive_data() %>%
+          summarise(count = n(), avg=mean(Value), median=median(Value), distinct_members = n_distinct(Subscription.Id))
+      })
+      
+      output$orders_render_graph_TOV <- renderGvis({
+        graph_data <- orders_reactive_data() %>% 
+          filter(Community.Type %in% c("WORKPLACE", "HOME", "SCHOOL")) %>%
+          group_by(order_week, Community.Type) %>%
+          summarise(Total=sum(Value)) %>%
+          spread(Community.Type, Total)
+        gvisLineChart(graph_data, xvar="order_week", yvar=orders_reactive_type())
+      })
+      
+      output$orders_render_graph_ordercount <- renderGvis({
+        graph_data <- orders_reactive_data() %>% 
+          filter(Community.Type %in% c("WORKPLACE", "HOME", "SCHOOL")) %>%
+          group_by(order_week, Community.Type) %>%
+          summarise(Count=n()) %>%
+          spread(Community.Type, Count)
+        gvisLineChart(graph_data, xvar="order_week", yvar=orders_reactive_type())
+      })
+      
+      output$orders_render_graph_communities <- renderGvis({
+        graph_data <- orders_reactive_data() %>% 
+          filter(Community.Type %in% c("WORKPLACE", "HOME", "SCHOOL")) %>%
+          group_by(order_week, Community.Type) %>%
+          summarise(Count = n_distinct(Community.Id)) %>%
+          spread(Community.Type, Count)
+        gvisLineChart(graph_data, xvar="order_week", yvar=orders_reactive_type())
+      })
+      
+    #Map Tab Output
+      output$orders_render_map_orders <- renderLeaflet({
+        #summarize data by long/lat
+        Map_Data<-orders_reactive_data() %>%
+          group_by(longitude, latitude, Zip.Code, Community.Type) %>%
+          summarize(TotalOrders=n(), TotalOrderValue=sum(Value))
+        
+        leaflet(Map_Data) %>% addTiles() %>%
+        #  addMarkers()
+        addCircleMarkers(
+          radius = 2,
+          popup = ~paste("ZIP CODE: ", Zip.Code, " - ", TotalOrders),
+         color = ~ifelse(Community.Type == "SCHOOL", "red","blue")
+        ) %>%
+          addLegend(position="bottomright", 
+                    colors = c("red", "blue"),
+                    labels = c("School", "Home/Workplace"))
+        
+      })
+      
+    #Download Tab Output
+      output$orders_render_table_rawdata <- renderDataTable({
+        orders_reactive_data()
+      })
+    
+
+
+                
+#
+#
+#Cohort TAB#  
+#                    
+#      
+      #-------------------------------------------------
+      #Reactive Functions (used by the render functions)
+      #-------------------------------------------------
+      
+      
+      cohort_reactive_timeframe <- reactive({
+        switch(input$cohort_input_timeframe,
+               "Week" = "mem_first_order_week",
+               "Month" = "mem_first_order_month",
+               "Annual" = "mem_first_order_year")
+      })
+      
+      cohort_reactive_region <- reactive({
+        switch(input$cohort_input_region,
+               "NY" = c("NY"),
+               "SF Bay" = c("SF Bay"),
+               "Both Regions" = c("NY","SF Bay"))  
+      })
+      
+      cohort_reactive_type <- reactive({
+        switch(input$cohort_input_type,
+               "School" = c("SCHOOL"),
+               "Home/Work" = c("HOME", "WORKPLACE"),
+               "All" = c("SCHOOL","HOME", "WORKPLACE"))
+      }) 
+      cohort_reactive_data <- reactive({
+        Members %>%
+          filter(Region %in% cohort_reactive_region(), Community.Type %in% cohort_reactive_type(), 
+                 (mem_first_order_week >= input$cohort_input_firstOrder[1] &
+                    mem_first_order_week <= input$cohort_input_firstOrder[2] )
+          )
+      })
+
+    
+      
+      #-------------------------------------------------
+      #Render Functions
+      #-------------------------------------------------   
+     output$cohort_render_AMOgraph <- renderGvis({
+        cohort_reactive_data() %>% 
+          filter(Community.Type %in% c("WORKPLACE", "HOME", "SCHOOL")) %>%
+          group_by_(cohort_reactive_timeframe()) %>%
+          summarise(AMO4=mean(count4, na.rm=TRUE), 
+                    AMO12=mean(count12, na.rm=TRUE),
+                    AMO24=mean(count24, na.rm=TRUE)) %>%
+        gvisLineChart(xvar=cohort_reactive_timeframe(), yvar=c("AMO4", "AMO12", "AMO24"))
+      })
+      
+      output$cohort_render_TMVgraph <- renderGvis({
+        cohort_reactive_data() %>% 
+          filter(Community.Type %in% c("WORKPLACE", "HOME", "SCHOOL")) %>%
+          group_by_(cohort_reactive_timeframe()) %>%
+          summarise(TMV4=mean(sum4, na.rm=TRUE), 
+                    TMV12=mean(sum12, na.rm=TRUE),
+                    TMV24=mean(sum24, na.rm=TRUE)) %>%
+          gvisLineChart(xvar=cohort_reactive_timeframe(), yvar=c("TMV4", "TMV12", "TMV24"))
+      })
+      
+      output$cohort_render_AMO4graph <- renderGvis({
+        orders_reactive_data <- cohort_reactive_data() %>% 
+          filter(Community.Type %in% c("WORKPLACE", "HOME", "SCHOOL"), count4<=4) %>%
+          group_by_(cohort_reactive_timeframe(), "count4") %>%
+          summarise(Count=n()) %>%
+          spread(count4, Count)
+        
+          orders_reactive_data[,2:5] <- orders_reactive_data[,2:5]/rowSums(orders_reactive_data[,2:5], na.rm=TRUE)
+          gvisColumnChart(orders_reactive_data, xvar=cohort_reactive_timeframe(), yvar=c("1","2","3","4"), 
+                        options=list(isStacked=TRUE))
+      })
+      
+})
---
+++ test.R
@@ -0,0 +1,127 @@
+
+library(shiny)  
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(zoo)
+library(googleVis)
+library(zipcode)
+setwd("/Users/shofujiwara/Dropbox/Farmigo/Shiny/RegionalDashboards/data")
+Orders <- as.data.frame(read.csv("toMergeOrders.csv", stringsAsFactors=FALSE, na.strings = "NA"))
+#change dates from strings-> dates
+Orders$First.order <- as.Date(Orders$First.order, format = "%Y-%m-%d")
+Orders$Last.order <- as.Date(Orders$Last.order, format = "%Y-%m-%d")
+Orders$Pick.up.Date <- as.Date(Orders$Pick.up.Date, format = "%Y-%m-%d")
+Orders$order_week <- as.Date(Orders$order_week, format = "%Y-%m-%d")
+class(Orders$order_week)
+Orders$order_month <- as.Date(as.yearmon(Orders$order_week))
+Orders$order_year <- format(Orders$order_week, format="%Y")
+
+data(zipcode)
+#For some reason, the NAs here in the Columns aren't real NAs, so we replace the "NA" with zeros this way
+Orders[c("Coupon", "Comp", "Ref.Receipt", "Ref.Sender")][is.na(Orders[c("Coupon", "Comp", "Ref.Receipt", "Ref.Sender")])] <- 0
+
+Orders$Zip.Code <- clean.zipcodes(Orders$Zip.Code)
+Orders <- inner_join(Orders, zipcode[c(1,4:5)], by = c("Zip.Code" = "zip"))
+CommunityWeeks <- as.data.frame(read.csv("toMergeCommunityWeeks.csv", stringsAsFactors=FALSE, na.strings = "NA"))
+CommunityWeeks$order_week <- as.Date(CommunityWeeks$order_week, format = "%Y-%m-%d")
+CommunityWeeks$order_month <- as.Date(as.yearmon(CommunityWeeks$order_week))
+CommunityWeeks$order_year <- format(CommunityWeeks$order_week, format="%Y")
+
+CommunityWeeks <- inner_join(CommunityWeeks, Orders[c("Community.Id", "latitude", "longitude")], by="Community.Id")
+head(CommunityWeeks)
+
+Orders <- inner_join(Orders, zipcode[c(1,4:5)], by = c("Zip.Code" = "zip"))
+
+Members <- as.data.frame(read.csv("toMergeMembers.csv", stringsAsFactors=FALSE, na.strings = "NA"))
+
+Members$mem_first_order_week <- as.Date(Members$mem_first_order_week, format = "%Y-%m-%d")
+Members$mem_last_order_week <- as.Date(Members$mem_last_order_week, format = "%Y-%m-%d")
+
+Members$mem_first_order_month <- as.Date(as.yearmon(Members$mem_first_order_week))
+Members$mem_first_order_year <- format(Members$mem_first_order_week, format="%Y")
+
+#-----------------
+devtools::install_github("rstudio/leaflet")
+library(leaflet)
+
+m <- leaflet() %>%
+  addTiles() %>% 
+  addMarkers(lng=174.768, lat=-36.852, popup = "The birthplae of R")
+m
+
+
+data(quakes)
+
+n <- leaflet(data = quakes[1:20,]) %>%
+  addTiles() %>%
+  addMarkers(~long, ~lat, popup = ~as.character(stations))
+
+
+
+
+quakes1 <- quakes[1:10,]
+quakes1$relativesize <- ifelse(quakes1$mag<5, "Small", "Big")
+
+leaflet(quakes1) %>% addTiles() %>%
+  addMarkers()
+
+leaflet(quakes1) %>% addTiles() %>%
+  addCircleMarkers(
+      radius = ~mag,
+     # popup = ~as.character(mag)
+     popup = ~paste(mag, stations, sep = "-"),
+     color = ~ifelse(relativesize == "Small", "red","navy")
+  )  %>%
+  addLegend(position="bottomright", 
+            colors = c("red", "navy"),
+            labels = c("Small", "Big"))
+
+
+
+leafIcons <- icons(
+  iconUrl = ifelse(quakes1$mag < 4.6,
+                   "http://leafletjs.com/docs/images/leaf-green.png",
+                   "http://leafletjs.com/docs/images/leaf-red.png"
+  ),
+  iconWidth = 38, iconHeight = 95,
+  iconAnchorX = 22, iconAnchorY = 94,
+  shadowUrl = "http://leafletjs.com/docs/images/leaf-shadow.png",
+  shadowWidth = 50, shadowHeight = 64,
+  shadowAnchorX = 4, shadowAnchorY = 62
+)
+
+
+
+leaflet(data = quakes1) %>% addTiles() %>%
+  addMarkers(~long, ~lat, icon = leafIcons)
+
+
+
+
+
+
+oceanIcons <- iconList(
+  ship = makeIcon("ferry-18.png", "ferry-18@2x.png", 18, 18),
+  pirate = makeIcon("danger-24.png", "danger-24@2x.png", 24, 24)
+)
+
+# Some fake data
+df <- sp::SpatialPointsDataFrame(
+  cbind(
+    (runif(20) - .5) * 10 - 90.620130,  # lng
+    (runif(20) - .5) * 3.8 + 25.638077  # lat
+  ),
+  data.frame(type = factor(
+    ifelse(runif(20) > 0.75, "pirate", "ship")
+  ))
+)
+
+leaflet(df) %>% addTiles() %>%
+  # Select from oceanIcons based on df$type
+  addMarkers(icon = ~oceanIcons[type])
+
+
+
+CommunityWeeks <- as.data.frame(read.csv("toMergeCommunityWeeks.csv", stringsAsFactors=FALSE, na.strings = "NA"))
+CommunityWeeks$order_week <- as.Date(CommunityWeeks$order_week, format = "%Y-%m-%d")
